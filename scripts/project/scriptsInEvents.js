1a11253b163ea14e6a45083f772f3a2f96dbfa0e
Matheus N <matheusnaschaves@gmail.com>
2022-08-18 16:04:11 +0200

    galactic shooter

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,24 @@
+var postMsg = ''
+
+function postText(value){
+	console.log('iFrame sender: ' + value) 
+	parent.postMessage(value, "*")
+}
+
+
+const scriptsInEvents = {
+
+		async GameEvsheet_Event23_Act4(runtime, localVars)
+		{
+			console.log(runtime.globalVars.TotalScore)
+		},
+
+		async GameEvsheet_Event164_Act3(runtime, localVars)
+		{
+			postText(runtime.globalVars.finalScore)
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
